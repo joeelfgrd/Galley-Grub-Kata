4d2565afb14e451e0f779752952069e39c3790e7
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-04-10 00:11:19 +0200

    Clase item creada

---
+++ app/src/main/java/org/example/Item.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Item {
+    String name ="";
+    Double price = 0d;
+    String extra ="";
+
+    public Item(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+}
