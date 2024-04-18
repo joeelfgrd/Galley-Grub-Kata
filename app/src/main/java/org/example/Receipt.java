6237d1c9ea552fd7c7de9ba13a2f964dcf7cb720
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-04-18 17:45:06 +0200

    Feat(Receipt.java):Creacion de los metodos necesarios

--- app/src/main/java/org/example/Receipt.java
+++ app/src/main/java/org/example/Receipt.java
@@ -1,8 +1,9 @@
 package org.example;
 
-public class Receipt {
+public class Receipt implements Ticket{
     private Double total = 0.0;
     private Comanda order;
+    private Extra firstExtra;
     public Receipt (Comanda order){
         this.order = order;
         this.total = order.getTotal();
@@ -10,11 +11,31 @@ public class Receipt {
     public Comanda getOrder(){
         return this.order;
     }
-    public Double getTotal(){
+    public Double getTotal(){ return this.total;
+    }
+    public void setChain(Extra extra) {
+        this.firstExtra = extra;
+    }
+
+    Extra getChain() {
+        return this.firstExtra;
+    }
+
+    public Double total() {
+        if (this.total == 0d) {
+            this.sumExtrasCharge();
+            this.total = this.getOrder().getTotal();
+        }
         return this.total;
     }
+
+    void sumExtrasCharge() {
+        if (this.firstExtra != null) {
+            this.firstExtra.sumExtras(this.order);
+        }
+    }
+
     public void print(){
-        System.out.println("\n\t --- RECEIPT --- ");
         this.order.display();
         System.out.println("\t Total: " + this.total);
     }
