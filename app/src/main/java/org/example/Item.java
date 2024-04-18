4c121becde3adb14781f0761d54541fd9c926a29
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-04-18 17:42:26 +0200

    Fix(Item.java): Añadidos los metodos que faltaban

--- app/src/main/java/org/example/Item.java
+++ app/src/main/java/org/example/Item.java
@@ -1,62 +1,73 @@
 package org.example;
 
 public class Item implements Product {
-    String name ="";
+    String name = "";
     Double price = 0d;
-    String extra ="";
+    String extra = "";
+    String priceString;
 
     public Item(String name, Double price) {
         this.name = name;
         this.price = price;
+        this.priceString = String.format("%.2f", price); 
     }
-    public Item(String name,Double price ,String extra){
-        this.name=name +  extra;
-        this.price=price;
-        this.extra=extra;
+
+    public Item(String name, Double price, String extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+        this.priceString = String.format("%.2f", price); 
     }
-    public String name(){
+
+    public String name() {
         return this.name;
-    }    
-    public Double price(){
+    }
+
+    public Double price() {
         return this.price;
-    }   
-    public String extra(){
+    }
+
+    public String extra() {
         return this.extra;
     }
-    public Boolean isRegular(){
+
+    public Boolean isRegular() {
         return this.extra().isBlank();
     }
 
-    @Override
-public String toString() {
-    StringBuilder menu = new StringBuilder();
-    menu.append(name);
-    menu.append("....");
-    menu.append(String.format("%.2f", price)); 
-    menu.append("$");
-    
-    if (extra != null && !extra.isEmpty()) {
-        StringBuilder menu_extra = new StringBuilder();
-        menu_extra.append(name);
-        menu_extra.append("  /w  ");
-        menu_extra.append(extra);
-        menu_extra.append(".........");
-        menu_extra.append(String.format("%.2f", price));
-        menu_extra.append("$\t");
-        return menu.toString() + menu_extra.toString();
-    } else {
-        return menu.toString();
+    public String getExtra(){
+        return this.extra;
     }
-}
 
-    
+    @Override
+    public String toString() {
+        StringBuilder menu = new StringBuilder();
+        if (extra != null && !extra.isEmpty()) {
+            StringBuilder menu_extra = new StringBuilder();
+            menu_extra.append(name);
+            menu_extra.append(" w/ ");
+            menu_extra.append(extra);
+            menu_extra.append("....");
+            menu_extra.append(priceString);
+            menu_extra.append("$ ");
+            menu_extra.append("+ ");
+            menu_extra.append(String.format("%.2f",Prices.getPrice(this.extra)));
+            menu_extra.append("$");
+            return menu_extra.toString();
+        } else {
+            menu.append(name);
+            menu.append("....");
+            menu.append(priceString); 
+            menu.append("$");
+            return menu.toString();
+        }
+    }
 
-
-    
     @Override
     public boolean equals(Object obj) {
         return (obj instanceof Item) && this.name.equals(((Item)obj).name);
     }
+
     @Override
     public int hashCode() {
         return this.name.hashCode();
