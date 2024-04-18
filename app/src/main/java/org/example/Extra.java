9d1152f47199325db5f1fcb45b0114841aabf921
Joel Figueirido <joelfigueiridomolares@gmail.com>
2024-04-18 17:40:32 +0200

    refactor(Extra.java):Optimizacion de la clase Extra

--- app/src/main/java/org/example/Extra.java
+++ app/src/main/java/org/example/Extra.java
@@ -1,5 +1,16 @@
 package org.example;
+import java.util.Optional;
 
-public class Extra {
+public abstract class Extra {
+    static final String CHEESE = "cheese";
+    static final String SAUCE = "sauce";
+    static final String SIZE_LARGE = "large";
+    protected String extraProduct;
+    Optional<Extra> nextExtra;
     
+    public Extra(){}
+    public void setNextExtra(Extra extra) {
+        this.nextExtra = Optional.ofNullable(extra);
+    }
+    public abstract void sumExtras(Comanda comanda);
 }
